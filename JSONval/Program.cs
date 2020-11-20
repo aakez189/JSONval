609b1e748cd36fab3d3c1aaa2fdc0a5a55d49925
aakez189 <aakez189@users.noreply.github.com>
2020-11-20 13:36:10 +0100

    bugfixing

--- JSONval/Program.cs
+++ JSONval/Program.cs
@@ -10,14 +10,13 @@ using Newtonsoft.Json.Schema;
 namespace JSONval {
 
 	internal class JsonValidator {
-		private readonly string _jsonFile;
-		private readonly string _schemaFile;
-		private const string DummySchema = @"'$schema' : 'https://json-schema.org/draft/2019-09/schema'";
+		private readonly string _jsonFile = null;
+		private readonly string _schemaFile = null;
+		
+		private const string DummySchema = @"{'$schema': 'https://json-schema.org/draft/2019-09/schema'}";
 
-		private JSchema jschema;
-		private JObject jobject;
-		private StreamReader streamreader;
-		private JsonTextReader textreader;
+		private StreamReader streamreader = null;
+		private JsonTextReader textreader = null;
 
 
 		/// <summary>
@@ -43,18 +42,20 @@ namespace JSONval {
 		/// </summary>
 		private JSchema JsonSchema {
 			get {
-				if (jschema == null)
-					using (streamreader = File.OpenText(_schemaFile))
-						try {
-							textreader = new JsonTextReader(streamreader) { 
-								CloseInput = true 
-							};
-							jschema = JSchema.Load(textreader);
-						}
-						catch (JsonReaderException error) { 
-							Console.WriteLine(error); 
-						}
-				return jschema;
+				if (_schemaFile.Equals(DummySchema))
+					return JSchema.Parse(_schemaFile);
+				
+				using (streamreader = File.OpenText(_schemaFile))
+					try {
+						textreader = new JsonTextReader(streamreader) { 
+							CloseInput = true 
+						};
+						return JSchema.Load(textreader);
+					}
+					catch (JsonReaderException error) { 
+						Console.WriteLine(error);
+						return null;
+					}
 			}
 		}
 
@@ -64,18 +65,17 @@ namespace JSONval {
 		/// </summary>
 		private JObject JsonFile {
 			get {
-				if (jobject == null)
-					using (streamreader = File.OpenText(_jsonFile))
-						try {
-							textreader = new JsonTextReader(streamreader) {
-								CloseInput = true
-							};
-							jobject = JToken.ReadFrom(textreader) as JObject;
-						}
-						catch (JsonReaderException error) {
-							Console.WriteLine(error);
-						}
-				return jobject;
+				using (streamreader = File.OpenText(_jsonFile))
+					try {
+						textreader = new JsonTextReader(streamreader) {
+							CloseInput = true
+						};
+						return JToken.ReadFrom(textreader) as JObject;
+					}
+					catch (JsonReaderException error) {
+						Console.WriteLine(error);
+						return null;
+					}
 			}
 		}
 
